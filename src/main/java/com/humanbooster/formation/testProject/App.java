20338e64e6dc6cfbef3a99cb5e3e1d7bb62148f5
Xavier Bauquet <xavier.bauquet@gmail.com>
2016-03-10 09:41:52 +0100

    ADD: project files

---
+++ src/main/java/com/humanbooster/formation/testProject/App.java
@@ -0,0 +1,13 @@
+package com.humanbooster.formation.testProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
