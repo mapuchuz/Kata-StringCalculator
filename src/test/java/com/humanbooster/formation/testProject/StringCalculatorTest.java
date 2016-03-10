ff6fb6a95eb049408e6710ecb2f5d71b5a2edba9
Vincent Demolombe <vincent.demolombe@sfr.fr>
2016-03-10 14:07:28 +0100

    Avec Before&After

--- src/test/java/com/humanbooster/formation/testProject/StringCalculatorTest.java
+++ src/test/java/com/humanbooster/formation/testProject/StringCalculatorTest.java
@@ -1,10 +1,22 @@
 package com.humanbooster.formation.testProject;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
 
+    @Before
+    public void avantTest() {
+        System.out.println("On va tester la classe StringCalculator");
+    }
+    
+    @After
+    public void apresTest() {
+        System.out.println("On va FINI les tests !");
+    }
+    
 	@Test
 	// Test if the method add return 0 with an empty string
 	public void shouldReturnZeroWhenStringEmpty() {
