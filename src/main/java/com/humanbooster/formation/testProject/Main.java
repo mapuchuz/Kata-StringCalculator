df3b01cfb0dfbf5726ec45cb3f090571aaaa25f8
Vincent Demolombe <vincent.demolombe@sfr.fr>
2016-03-11 15:11:59 +0100

    cobertura

--- src/main/java/com/humanbooster/formation/testProject/Main.java
+++ src/main/java/com/humanbooster/formation/testProject/Main.java
@@ -1,15 +1,30 @@
 package com.humanbooster.formation.testProject;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//
+//		StringCalculator calc = new StringCalculator();
+//		System.out.println(calc.add("12,14"));
+//		System.out.println(calc.add("12,14frf"));
 
-			StringCalculator calc = new StringCalculator();
-			System.out.println( 			calc.add("12,14") );
-			System.out.println( 			calc.add("12,14frf") );
+		String toto = "//[***][;;]\n1***2***3;;4";
+		Pattern pattern = Pattern.compile("//(\\[(.+)\\])+\\s(.+)");
+		Matcher matcher = pattern.matcher(toto);
 
-
+		if (matcher.find()) {
+			System.out.println( matcher.group(1));
+			System.out.println( matcher.group(2));
+			System.out.println( matcher.group(3));
+			System.out.println("");
+			String[] tab = matcher.group(2).split(Pattern.quote(matcher.group(1)));
+			for (String t : tab)
+				System.out.println(t);
+		}
 
 	}
 
